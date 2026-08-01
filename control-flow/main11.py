3676943c450fcf3be675bb752ae0e214f04b1b85
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-01 13:11:55 +0530

    added control-flow folder

---
+++ control-flow/main11.py
@@ -0,0 +1,27 @@
+# a = int(input("enter your age:"))
+# print("your age is:",a)
+
+# if(a>18):
+#     print("you can drive")
+# else:
+#     print("you cannot drive")
+
+# appleprice = 210
+# budget = 200
+# if(appleprice<=budget):
+#     print("yes give me 1 kg of apples")
+# else:
+#     print("no i dont want apples")
+
+num = int(input("enter the value of num: "))
+if(num<0):
+    print("num is negative")
+elif(num==0):
+    print("the number is 0")
+else:
+    (num>0)
+print("the num is positive")
+
+input("\n")
+print("i am happy now")
+
