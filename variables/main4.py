5e875f8da41898780cced2b5a6eecbeef88c50d1
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-01 13:17:35 +0530

    added variables folder

---
+++ variables/main4.py
@@ -0,0 +1,11 @@
+a = 40
+# print(a)
+a1 = 9 
+b = "vansh"
+print(b)
+print (a+a1)
+print("type of a is", type(a))
+print("type of b is", type(b))
+print("type of a1 is", type(a1))
+e = complex(8+3)
+print(e)
