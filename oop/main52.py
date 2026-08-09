a23d7dfebdb6d110fd8e000f61d482cf2b0c6665
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-09 17:39:25 +0530

    learned super() keyword

--- oop/main52.py
+++ oop/main52.py
@@ -1,10 +1,39 @@
-class person:
+# USING OF DIR() AND __DICT AND MAP() FOR INTEROSPECTION
+
+# class person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+# a = person("vansh","19")
+# print(a.name,a.age)
+# print(dir(a))
+# print(a.__dict__)
+# print(help(person))
+
+
+# USING SUPER() KEYWORD
+
+class Employee:
     def __init__(self,name,age):
-        self.name = name
+        self.name = name 
         self.age = age
 
-a = person("vansh","19")
-print(a.name,a.age)
-print(dir(a))
-print(a.__dict__)
-print(help(person))
+class Student(Employee):
+    def __init__(self,name,age,language):
+        super().__init__(name,age)
+        self.language = language
+
+a = Employee("umang","18")
+print(a.name)
+print(a.age)
+b = Student("umang","18","python")
+print(b.name)
+print(b.age)
+print(b.language)
+
+
+# a = Employee("umang","18")
+# a()
+# b = Student("umang","18","python")
+# b()
