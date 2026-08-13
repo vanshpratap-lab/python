a534b2b591af3c121c06289f7cd0a4706d0a042a
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-13 22:20:57 +0530

    modified the code

--- oop/main52.py
+++ oop/main52.py
@@ -1,36 +1,36 @@
-# USING OF DIR() AND __DICT AND MAP() FOR INTEROSPECTION
+# USING OF DIR() AND __DICT__ AND MAP() FOR INTEROSPECTION
 
-# class person:
-#     def __init__(self,name,age):
-#         self.name = name
-#         self.age = age
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
 
-# a = person("vansh","19")
-# print(a.name,a.age)
-# print(dir(a))
-# print(a.__dict__)
-# print(help(person))
+a = person("vansh","19")
+print(a.name,a.age)
+print(dir(a))
+print(a.__dict__)
+print(help(person))
 
 
 # USING SUPER() KEYWORD
 
-class Employee:
-    def __init__(self,name,age):
-        self.name = name 
-        self.age = age
+# class Employee:
+#     def __init__(self,name,age):
+#         self.name = name 
+#         self.age = age
 
-class Student(Employee):
-    def __init__(self,name,age,language):
-        super().__init__(name,age)
-        self.language = language
+# class Student(Employee):
+#     def __init__(self,name,age,language):
+#         super().__init__(name,age)
+#         self.language = language
 
-a = Employee("umang","18")
-print(a.name)
-print(a.age)
-b = Student("umang","18","python")
-print(b.name)
-print(b.age)
-print(b.language)
+# a = Employee("umang","18")
+# print(a.name)
+# print(a.age)
+# b = Student("umang","18","python")
+# print(b.name)
+# print(b.age)
+# print(b.language)
 
 
 # a = Employee("umang","18")
