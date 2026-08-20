65352b61b6044fe73d03f761f122e5d94e4dbcd1
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-20 21:51:07 +0530

    used operator overloading

--- oop/main55.py
+++ oop/main55.py
@@ -7,8 +7,13 @@ class operator:
     def __str__(self):
         return (f"{self.i}i + {self.j}j + {self.k}k")
 
-v1 = operator(1,2,3)
-# print(v1)
+    def __add__(self,x):
+        return self.__class__(self.i + x.i, self.j + x.j, self.k + x.k)
 
+v1 = operator(1,2,3)
+print(v1)
+v2 = operator(4,5,6)
+print(v2)
+print(v1 + v2)
         
 
