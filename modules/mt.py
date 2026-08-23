5a249c5add50771ed43fcd6cfb6867591c4d0d71
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-23 19:14:00 +0530

    used threading module

--- modules/mt.py
+++ modules/mt.py
@@ -12,9 +12,15 @@ func(4)
 func(2)
 func(1)
 
+time1 = time.perf_counter()
+
 t1 = threading.Thread(target=func, args=[4])
 t2 = threading.Thread(target=func, args=[2])
 t3 = threading.Thread(target=func, args=[1])
+
+time2 = time.perf_counter()
+
+print(time2 - time1)
 
 t1.start()
 t2.start()
