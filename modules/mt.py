8462a9130a76cfd879106841b53783c069bf016b
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-23 19:04:59 +0530

    used threading module

---
+++ modules/mt.py
@@ -0,0 +1,21 @@
+# -------multithreading module------------
+# multithreading module is usedlike when you wanna downlaod things parallely togetehr rather then one by one to exefute tasks togetehr ratehr then oneby one you use mulithreading
+
+import threading 
+import time
+
+def func(seconds):
+    print(f"sleeping for {seconds} seconds")
+    time.sleep(seconds)
+
+func(4)
+func(2)
+func(1)
+
+t1 = threading.Thread(target=func, args=[4])
+t2 = threading.Thread(target=func, args=[2])
+t3 = threading.Thread(target=func, args=[1])
+
+t1.start()
+t2.start()
+t3.start()
