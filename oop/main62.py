f9548c12b69474dcf331527d3b5e557e8b06c062
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-23 17:14:02 +0530

    used functools module for cache problem

---
+++ oop/main62.py
@@ -0,0 +1,23 @@
+# lru_cahce works for memoization - stores the value and repeats it faslty whnerve in use afterwards rahter then doing the full process
+
+from functools import lru_cache
+import time
+
+@lru_cache(maxsize = None)
+def fx(n):
+    time.sleep(5)
+    return n*5
+
+print(fx(20))
+print("done for 20")
+print(fx(6))
+print("done for 6")
+print(fx(2))
+print("done for 2")
+
+print(fx(20))
+print("done for 20")
+print(fx(6))
+print("done for 6")
+print(fx(2))
+print("done for 2")
