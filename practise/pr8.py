3c17d8eea6ec6f6c5b832546975afdba4b8b32c5
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-27 23:05:56 +0530

    function for counting no. of alphabet in a string

---
+++ practise/pr8.py
@@ -0,0 +1,12 @@
+# from collections import Counter
+
+# user_input = input("please enter your sentence : ")
+# frequency = Counter(user_input)
+
+# print(f"no. of times a appeard : {frequency['a']}")
+
+user_input = input("please say soemthing : ")
+
+counter = user_input.count('a')
+
+print(counter)
