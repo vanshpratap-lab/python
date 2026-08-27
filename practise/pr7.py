32365379817702c96eca34e3c848769987d86475
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-27 23:04:49 +0530

    using loop and for converting celsius to fahenheit

---
+++ practise/pr7.py
@@ -0,0 +1,37 @@
+# USING LOOP METHOD
+# temprature_celsius = [20,30,40,50,60]
+# temrpature_fahenheit = []
+# for i in temprature_celsius:
+#     conversion =  ( i * 1.8 ) + 32 
+#     temrpature_fahenheit.append(conversion)
+
+# print(temrpature_fahenheit)
+
+
+
+
+
+
+
+
+
+
+#  USING LOOP USER INPUT METHOD
+
+# temrpature_fahrenheit = []
+# temrpature_celsius = int(input("please enter your temprature : "))
+
+# # temp = int(input("please enter your input : "))
+
+# # for i in range(1):
+#     # temrpature_celsius.append(temp)
+# for temp in temrpature_celsius:
+#     conversion = ( temp * 1.8 ) + 32
+#     temrpature_fahrenheit.append(conversion)
+# # print(temrpature_celsius)
+# print(temrpature_fahrenheit)
+
+temprature = int(input("please enter your temrpature : "))
+
+conversion = (temprature * 1.8) + 32
+print(conversion)
